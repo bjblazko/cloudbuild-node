d66ad50e79b74bee3b8425d862495b737d318e95
Timo Böwing <bjblazko@users.noreply.github.com>
2022-04-22 18:14:12 +0200

    Update function.js

--- hello/function.js
+++ hello/function.js
@@ -1,7 +1,3 @@
-const functions = require('@google-cloud/functions-framework');
-const escapeHtml = require('escape-html');
-
-// HTTP Cloud Function.
-functions.http('helloHttp', (req, res) => {
-  res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
+exports.hello = (req, res) => {
+  res.send(`Hello ${Date.now()}!`);
 });
